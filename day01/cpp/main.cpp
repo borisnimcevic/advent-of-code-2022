a9fd5ab0da8847b15806a8c53868e16cd0c56153
Boris Nimcevic <borisnimcevic@gmail.com>
2022-12-02 00:37:39 +0100

    Day 1 cpp

---
+++ day01/cpp/main.cpp
@@ -0,0 +1,35 @@
+#include <fstream>
+#include <iostream>
+
+int main() {
+  std::ifstream file("../input.txt");
+
+  if (!file) {
+    std::cerr << "Cannot find the file." << std::endl;
+  }
+
+  std::string line;
+  int sum = 0;
+  int max1 = 0;
+  int max2 = 0;
+  int max3 = 0;
+  while (std::getline(file, line)) {
+    if (line == "") {
+      if (sum > max1) {
+        max3 = max2;
+        max2 = max1;
+        max1 = sum;
+      } else if (sum > max2) {
+        max3 = max2;
+        max2 = sum;
+      } else if (sum > max3) {
+        max3 = sum;
+      }
+      sum = 0;
+    } else {
+      sum += std::stoi(line);
+    }
+  }
+  std::cout << max1 << std::endl;
+  std::cout << max1 + max2 + max3 << std::endl;
+}
