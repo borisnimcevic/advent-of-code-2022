39fa357671a6203404393ab655bcb6244a2544a9
Boris Nimcevic <borisnimcevic@gmail.com>
2022-12-06 21:24:10 +0100

    making cpp a bit more readable

--- day06/cpp/main.cpp
+++ day06/cpp/main.cpp
@@ -2,20 +2,23 @@
 #include <iostream>
 #include <set>
 
-uint32_t findMatch_better(const std::string *const input,
-                          const uint8_t offset) {
+uint32_t findMatch(const std::string *const input, const uint8_t offset) {
   std::set<char> char_set;
+
   for (auto index = 0; index < input->size(); index++) {
-    if (char_set.end() == char_set.find(input->at(index))) {
-      char_set.insert(input->at(index));
-      if (char_set.size() == offset) {
-        return index + 1; // becasue the solution 1-indexed
-      }
-    } else {
+
+    if (char_set.end() != char_set.find(input->at(index))) {
       index -= char_set.size();
       char_set.clear();
+      continue;
+    }
+
+    char_set.insert(input->at(index));
+    if (char_set.size() == offset) {
+      return index + 1; // becasue the solution is 1-indexed
     }
   }
+
   return 0;
 }
 
@@ -29,6 +32,6 @@ int main() {
 
   std::string line;
   std::getline(file, line);
-  std::cout << "part one: " << findMatch_better(&line, 4) << std::endl;
-  std::cout << "part two: " << findMatch_better(&line, 14) << std::endl;
+  std::cout << "part one: " << findMatch(&line, 4) << std::endl;
+  std::cout << "part two: " << findMatch(&line, 14) << std::endl;
 }
